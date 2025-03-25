cc2f4177daa2c76dbdcd3ded7f7394a708ce09fe
Michael Dreesen <mdreesen@Mac-840.lan>
2025-03-25 15:09:11 -0600

    fix(homepage)

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,12 @@ import {
   CloudArrowUpIcon,
   FingerPrintIcon,
   LockClosedIcon,
+  MinusSmallIcon,
+  PlusSmallIcon,
 } from '@heroicons/react/24/outline'
 import ButtonGoTo from '@/components/buttons/ButtonGoTo';
 import ButtonGoToArrow from '@/components/buttons/ButtonGoToArrow';
+import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/react';
 
 const features = [
   {
@@ -242,20 +245,30 @@ export default function Page() {
         </div>
 
         {/* FAQs */}
-        <div className="mx-auto max-w-2xl px-6 py-8 sm:pb-24 sm:pt-12 lg:max-w-7xl lg:px-8 lg:pb-32">
-          <h2 className="text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
-            Frequently asked questions
-          </h2>
-          <dl className="mt-20 divide-y divide-gray-900/10">
-            {faqs.map((faq) => (
-              <div key={faq.id} className="py-8 first:pt-0 last:pb-0 lg:grid lg:grid-cols-12 lg:gap-8">
-                <dt className="text-base/7 font-semibold text-gray-900 lg:col-span-5">{faq.question}</dt>
-                <dd className="mt-4 lg:col-span-7 lg:mt-0">
-                  <p className="text-base/7 text-gray-600">{faq.answer}</p>
-                </dd>
-              </div>
-            ))}
-          </dl>
+        <div className="mx-auto mt-22 pb-20 max-w-7xl px-6 sm:mt-26 lg:px-8">
+          <div className="mx-auto max-w-4xl">
+            <h2 className="text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
+              Frequently asked questions
+            </h2>
+            <dl className="mt-16 divide-y divide-gray-900/10">
+              {faqs.map((faq) => (
+                <Disclosure key={faq.question} as="div" className="py-6 first:pt-0 last:pb-0">
+                  <dt>
+                    <DisclosureButton className="group flex w-full items-start justify-between text-left text-gray-900">
+                      <span className="text-base/7 font-semibold">{faq.question}</span>
+                      <span className="ml-6 flex h-7 items-center">
+                        <PlusSmallIcon aria-hidden="true" className="size-6 group-data-[open]:hidden" />
+                        <MinusSmallIcon aria-hidden="true" className="size-6 group-[&:not([data-open])]:hidden" />
+                      </span>
+                    </DisclosureButton>
+                  </dt>
+                  <DisclosurePanel as="dd" className="mt-2 pr-12">
+                    <p className="text-base/7 text-gray-600">{faq.answer}</p>
+                  </DisclosurePanel>
+                </Disclosure>
+              ))}
+            </dl>
+          </div>
         </div>
       </main>
     </div>
